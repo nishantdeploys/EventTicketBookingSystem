aa7d05c6d93242e1c44e5474251ad2f9c9b94b36
Nishant Kumar <nishant098097@gmail.com>
2025-08-14 22:14:50 +0530

    Updated User.h

--- include/User.h
+++ include/User.h
@@ -1,18 +1,15 @@
 #pragma once
 #include <string>
-using namespace std;
 
 class User {
-    string userID, name, email;
-    bool isVIP;
+private:
+    std::string username;
+    std::string password;
+    bool admin;
 public:
     User();
-    User(const string& id, const string& n, const string& e, bool vip);
-    // Getters
-    string getUserID() const;
-    string getName() const;
-    string getEmail() const;
-    bool getVIPStatus() const;
-    // Setters
-    void setVIPStatus(bool vip);
+    bool login();
+    void registerUser();
+    bool isAdmin() const;
+    void showUserMenu();
 };
